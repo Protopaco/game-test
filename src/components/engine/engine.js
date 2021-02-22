b9932e6bf7dceb95f8302fcd898d53bb8e5cb6fa
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-22 14:44:44 -0800

    skateboard

--- src/components/engine/engine.js
+++ src/components/engine/engine.js
@@ -1,16 +1,70 @@
-import { useEventCallback } from '@material-ui/core';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './engine.module.scss';
-import { useDidMount } from '../../hooks/useEvent';
+import { useEvent } from '../../hooks/';
 
 export default function Engine() {
-    const handleKeyPress = (e) => { };
+    // const [engine, setEngine] = useState(null);
+    const [playerPosition, setPlayerPosition] = useState({ x: 0, y: 0 });
 
-    useEventCallback('keyup', handleKeyPress);
+    const handleKeyPress = (e) => {
+        e.preventDefault();
+
+        if (e.key === 'ArrowUp') {
+            const newPlayerPosition = {
+                x: playerPosition.x,
+                y: playerPosition.y - 25
+            };
+            setPlayerPosition(newPlayerPosition);
+        }
+
+        if (e.key === 'ArrowDown') {
+            const newPlayerPosition = {
+                x: playerPosition.x,
+                y: playerPosition.y + 25
+            };
+            setPlayerPosition(newPlayerPosition);
+        }
+
+        if (e.key === 'ArrowRight') {
+            const newPlayerPosition = {
+                x: playerPosition.x + 25,
+                y: playerPosition.y
+            };
+            setPlayerPosition(newPlayerPosition);
+        }
+
+        if (e.key === 'ArrowLeft') {
+            const newPlayerPosition = {
+                x: playerPosition.x - 25,
+                y: playerPosition.y
+            };
+            setPlayerPosition(newPlayerPosition);
+        }
+    };
+
+    useEvent('keydown', handleKeyPress);
+
+    function gameloop() {
+
+        requestAnimationFrame(gameloop);
+    }
+
+    useEffect(() => {
+        const anim = requestAnimationFrame(gameloop);
+
+        return () => {
+            cancelAnimationFrame(anim);
+        };
+    }, [playerPosition]);
 
     return (
         <div className={styles.container}>
-            <span className={styles.character} />
+            <span
+                className={styles.character}
+                style={{
+                    transform: `translate(${playerPosition.x}px, ${playerPosition.y}px)`
+                }}
+            />
         </div>
     );
 }
