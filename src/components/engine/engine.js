61479fe5312072dbaee16159b8c57003d1f50423
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-22 11:14:29 -0800

    add useEvent hook

--- src/components/engine/engine.js
+++ src/components/engine/engine.js
@@ -1,9 +1,16 @@
+import { useEventCallback } from '@material-ui/core';
 import React from 'react';
+import styles from './engine.module.scss';
+import { useDidMount } from '../../hooks/useEvent';
 
 export default function Engine() {
+    const handleKeyPress = (e) => { };
+
+    useEventCallback('keyup', handleKeyPress);
+
     return (
-        <h1>
-            TEST TEST TEST
-        </h1>
+        <div className={styles.container}>
+            <span className={styles.character} />
+        </div>
     );
 }
---
+++ src/hooks/useEvent.js
@@ -0,0 +1,12 @@
+
+import { useEffect } from 'react';
+
+export default function useEvent(event, handler) {
+    useEffect(() => {
+        window.addEventListener(event, handler);
+
+        return function cleanup() {
+            window.removeEventListener(event, handler);
+        };
+    });
+}
